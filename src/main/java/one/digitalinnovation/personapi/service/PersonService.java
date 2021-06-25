9d9d21e70464833e72ee432a81dc192709d3af95
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2021-06-25 17:47:55 -0300

    Developed get person by Id

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 
@@ -39,6 +40,12 @@ public class PersonService {
 		return allPeople.stream()
 				.map(personMapper::toDTO)
 				.collect(Collectors.toList());
+	}
+	
+	public PersonDTO findById(Long id) throws PersonNotFoundException{
+		Person person = personRepository
+				.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
+		return personMapper.toDTO(person);
 	}
 
 }
