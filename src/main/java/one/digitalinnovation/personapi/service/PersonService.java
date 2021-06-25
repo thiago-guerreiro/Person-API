2de0e14ecbc1fc7c7036ea1e719f9a9a05407e87
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2021-06-25 15:39:26 -0300

    Developed person create operation

---
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -0,0 +1,28 @@
+package one.digitalinnovation.personapi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+
+@Service
+public class PersonService {
+	
+	private PersonRepository personRepository;
+	
+	@Autowired
+	public PersonService(PersonRepository personRepository) {
+		this.personRepository = personRepository;
+	}
+	
+	public MessageResponseDTO createPerson(Person person) {
+    	Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+        		.builder()
+        		.message("Created  person with ID " + savedPerson.getId()) 
+        		.build();
+    }
+
+}
